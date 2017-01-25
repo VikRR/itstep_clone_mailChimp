219fd3e4b91c3ff705915dae43a12c8a1dbde138
VikRR <rubetc.v.i@gmail.com>
2017-01-25 10:51:00 +0200

    fix login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,10 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
+                <div class="text-center">
+                    <h1>Login</h1>
+                </div>
                 <div class="panel panel-default">
-                    <div class="panel-heading text-center">Login</div>
                     <div class="panel-body">
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                             {{ csrf_field() }}
@@ -38,29 +40,17 @@
                                 </div>
                             </div>
 
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}}> Remember Me
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
 
                             <div class="form-group">
-                                <div class="col-md-8 col-md-offset-4">
+                                <div class="col-md-6 col-md-offset-4 text-center">
                                     <button type="submit" class="btn btn-primary">
                                         Login
                                     </button>
 
-                                    <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                        Forgot Your Password?
-                                    </a>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <div class="col-md-8 col-md-offset-4 text-center">
+                                <div class="col-md-6 col-md-offset-4 text-center">
                                     <a href="{{ url('/register') }}">Or Register</a>
                                 </div>
                             </div>
