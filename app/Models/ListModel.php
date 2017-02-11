272a89be43eab91dcebae90d90a0190302a8b33f
VikRR <rubetc.v.i@gmail.com>
2017-02-11 09:18:17 +0200

    class work

---
+++ app/Models/ListModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ListModel extends Model
+{
+	use SoftDeletes;
+	
+    protected $table = 'lists';
+    
+    protected $fillable = ['user_id','name'];
+    
+    
+}
