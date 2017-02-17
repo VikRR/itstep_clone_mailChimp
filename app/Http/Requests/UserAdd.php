61821e5d0d59d07fe147c665ea02e3fc5d58f24e
VikRR <rubetc.v.i@gmail.com>
2017-02-17 18:41:35 +0200

    add validation user

---
+++ app/Http/Requests/UserAdd.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserAdd extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|max:128|min:2',
+            'last_name'  => 'required|max:128|min:2',
+            'email'      => 'required|email|max:128',
+        ];
+    }
+}
